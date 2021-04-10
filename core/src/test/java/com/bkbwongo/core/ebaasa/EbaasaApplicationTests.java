4be10eaf58ad0ebc5a97a596341c2d0a188c241c
bkaaron <baaronlubega1@gmail.com>
2021-04-10 13:54:37 +0300

    add circle ci config file

--- core/src/test/java/com/bkbwongo/core/ebaasa/EbaasaApplicationTests.java
+++ core/src/test/java/com/bkbwongo/core/ebaasa/EbaasaApplicationTests.java
@@ -3,11 +3,14 @@ package com.bkbwongo.core.ebaasa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class EbaasaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isTrue();
 	}
 
 }
