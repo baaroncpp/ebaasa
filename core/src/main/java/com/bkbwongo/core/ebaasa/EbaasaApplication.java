bf40bd1b43822f03960445ee32a800050153f185
bkaaron <baaronlubega1@gmail.com>
2021-04-10 13:15:18 +0300

    initial commit

---
+++ core/src/main/java/com/bkbwongo/core/ebaasa/EbaasaApplication.java
@@ -0,0 +1,13 @@
+package com.bkbwongo.core.ebaasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbaasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbaasaApplication.class, args);
+	}
+
+}
---
+++ core/src/test/java/com/bkbwongo/core/ebaasa/EbaasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkbwongo.core.ebaasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbaasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
