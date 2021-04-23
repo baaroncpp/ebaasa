a0574984242b4934e38a1350056f852c307363b8
bkaaron <baaronlubega1@gmail.com>
2021-04-23 18:22:08 +0300

    fix No identifier on Base Entity

--- core/src/main/java/com/bkbwongo/core/ebaasa/jpa/models/BaseEntity.java
+++ core/src/main/java/com/bkbwongo/core/ebaasa/jpa/models/BaseEntity.java
@@ -1,7 +1,8 @@
 package com.bkbwongo.core.ebaasa.jpa.models;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * @author bkaaron
@@ -11,6 +12,36 @@ import java.time.LocalDate;
 @MappedSuperclass
 public class BaseEntity {
     private String id;
-    private LocalDate createdOn;
-    private LocalDate modifiedOn;
+    private Date createdOn;
+    private Date modifiedOn;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column(name = "created_on",insertable =false, updatable = false)
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    @Column(name = "modified_on")
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    public Date getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(Date modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
 }
